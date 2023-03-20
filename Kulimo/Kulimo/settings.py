10287908f6851593e3a5e3790ab755cd765d8d61
bmorin2913 <123409397+bmorin2913@users.noreply.github.com>
2023-03-20 15:37:15 -0400

    404 et testing

--- Kulimo/Kulimo/settings.py
+++ Kulimo/Kulimo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4c%d@(3#x%t3tqfi*m!q=txzp-iah)%oo2r#b=ny&$!h_lgxnx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- Kulimo/accueil/urls.py
+++ Kulimo/accueil/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.main, name='main'),
     path('accueil/', views.accueil, name='accueil'),
     path('details/<int:id>', views.details, name='details'),
+    path('testing/', views.testing, name='testing'),
 ]
