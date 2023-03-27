4de196b62433fa6085f232aff776e5b91c77b9db
zekaran30 <zekaran30@gmail.com>
2023-03-27 13:10:43 -0400

    image

--- Kulimo/Kulimo/settings.py
+++ Kulimo/Kulimo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4c%d@(3#x%t3tqfi*m!q=txzp-iah)%oo2r#b=ny&$!h_lgxnx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*','localhost']
 
